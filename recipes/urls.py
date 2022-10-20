fa4bc5c416897982eeee2ca5c3cc66435cd078d3
cemimi27 <carloseduardomimi@gmail.com>
2022-10-20 18:42:56 -0300

    Inserção de dados aleatórios em recipe, com faker

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from recipes import views
-from recipes.views import home
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
