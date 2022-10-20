ef82ce1a2a171c43561ab6f52095208724aa057b
cemimi27 <carloseduardomimi@gmail.com>
2022-10-20 16:23:14 -0300

    base.html, recipe-view.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from recipes import views
 from recipes.views import home
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
