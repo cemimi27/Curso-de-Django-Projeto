9036d5d872f4d995f9e5326cb2cbe3a3c1a54c92
cemimi27 <carloseduardomimi@gmail.com>
2022-10-30 16:14:31 -0300

    Introdução ao models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)  # CharField(VARCHAR DO SQL)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()  # IntergerField(INT DO SQL)
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()  # IntergerField(INT DO SQL)
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()  # NÃO TEM LIMITE DE CARACTERE
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
