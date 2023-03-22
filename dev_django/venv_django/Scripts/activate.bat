8f45aa2fed0974db56a1375fcd06fb9d52501a8a
cemimi27 <carloseduardomimi@gmail.com>
2023-03-22 20:16:32 -0300

    Projeto Arrumado

--- dev_django/venv_django/Scripts/activate.bat
+++ dev_django/venv_django/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\DeskTop\Curso de Django\Projeto\dev_django
+set VIRTUAL_ENV=C:\Users\Carlos Eduardo Mimi\Desktop\Curso de Django\Projeto\dev_django\venv_django
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(dev_django) %PROMPT%
+set PROMPT=(venv_django) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(dev_django) 
+set VIRTUAL_ENV_PROMPT=(venv_django) 
 
 :END
 if defined _OLD_CODEPAGE (
